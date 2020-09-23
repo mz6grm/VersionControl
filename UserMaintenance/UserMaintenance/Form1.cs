952c72bb180d0e17f6406b4957875dc7b273dd20
Csaba Kaló <kalo.csk@gmail.com>
2020-09-23 23:50:11 +0200

    savefiledialog

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using UserMaintenance.Entities;
+using System.IO;
 
 namespace UserMaintenance
 {
@@ -20,6 +21,7 @@ namespace UserMaintenance
 			lblLastName.Text = Resource1.LastName; // label1
 			//lblFirstName.Text = Resource1.FirstName; // label2
 			btnAdd.Text = Resource1.Add; // button1
+			btnWrite.Text = Resource1.Write;
 
 			listUsers.DataSource = users;
 			listUsers.ValueMember = "ID";
@@ -33,6 +35,25 @@ namespace UserMaintenance
 				FullName = txtLastName.Text							
 			};
 			users.Add(u);
+		}
+
+		private void btnWrite_Click(object sender, EventArgs e)
+		{
+			SaveFileDialog sfd = new SaveFileDialog();
+			sfd.InitialDirectory = Application.StartupPath;
+			sfd.Filter = "Comma Seperated Values(*.csv)|*.csv";
+			sfd.DefaultExt = "csv";
+			sfd.AddExtension = true;
+			if (sfd.ShowDialog() != DialogResult.OK) return;
+
+			using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+			{
+				foreach (var s in users)
+				{
+					sw.Write(s.ID);
+					sw.Write(s.FullName);
+				}
+			}
 		}
 	}
 }
