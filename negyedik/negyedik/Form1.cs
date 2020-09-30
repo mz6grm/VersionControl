ff252f8f1d6831256cb2bc12604dffc7134caf41
Csaba Kaló <kalo.csk@gmail.com>
2020-09-30 18:57:05 +0200

    third commit

--- negyedik/negyedik/Form1.cs
+++ negyedik/negyedik/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace negyedik
 {
@@ -14,14 +16,125 @@ namespace negyedik
 	{
 		RealEstateEntities context = new RealEstateEntities();
 		List<Flat> Flats;
+		Excel.Application xlApp;
+		Excel.Workbook xlWB;
+		Excel.Worksheet xlSheet;
+
 		public Form1()
 		{
 			InitializeComponent();
 			LoadData();
+			try
+			{
+				// Excel elindítása és az applikáció objektum betöltése
+				xlApp = new Excel.Application();
+
+				// Új munkafüzet
+				xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+				// Új munkalap
+				xlSheet = xlWB.ActiveSheet;
+
+				// Tábla létrehozása
+				CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+				// Control átadása a felhasználónak
+				xlApp.Visible = true;
+				xlApp.UserControl = true;
+			}
+			catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+			{
+				string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+				MessageBox.Show(errMsg, "Error");
+
+				// Hiba esetén az Excel applikáció bezárása automatikusan
+				xlWB.Close(false, Type.Missing, Type.Missing);
+				xlApp.Quit();
+				xlWB = null;
+				xlApp = null;
+			}
+
 		}
 		private void LoadData()
 		{
 			Flats = context.Flat.ToList();
 		}
+		private void CreateTable()
+		{
+			string[] headers = new string[] 
+			{
+				"Kód",
+				"Eladó",
+				"Oldal",
+				"Kerület",
+				"Lift",
+				"Szobák száma",
+				"Alapterület (m2)",
+				"Ár (mFt)",
+				"Négyzetméter ár (Ft/m2)"
+			};
+			for (int i = 0; i < headers.Length; i++)
+			{
+				xlSheet.Cells[1, 1] = headers[0];
+				xlSheet.Cells[1, 2] = headers[1];
+				xlSheet.Cells[1, 3] = headers[2];
+				xlSheet.Cells[1, 4] = headers[3];
+				xlSheet.Cells[1, 5] = headers[4];
+				xlSheet.Cells[1, 6] = headers[5];
+				xlSheet.Cells[1, 7] = headers[6];
+				xlSheet.Cells[1, 8] = headers[7];
+				xlSheet.Cells[1, 9] = headers[8];
+			}
+			object[,] values = new object[Flats.Count, headers.Length];
+			int counter = 0;
+			foreach (Flat f in Flats)
+			{
+				values[counter, 0] = f.Code;
+				values[counter, 1] = f.Vendor;
+				values[counter, 2] = f.Side;
+				values[counter, 3] = f.District;
+				values[counter, 4] = f.Elevator;
+				values[counter, 5] = f.NumberOfRooms;
+				values[counter, 6] = f.FloorArea;
+				values[counter, 7] = f.Price;
+				values[counter, 8] = "";
+				counter++;
+			}
+			xlSheet.get_Range(
+			 GetCell(2, 1),
+			 GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+			//innen hiányzik valami
+
+			Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+			headerRange.Font.Bold = true;
+			headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+			headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+			headerRange.EntireColumn.AutoFit();
+			headerRange.RowHeight = 40;
+			headerRange.Interior.Color = Color.LightBlue;
+			headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+			//itt még folytatódik a feladat
+		}
+
+		private string GetCell(int x, int y)
+		{
+			string ExcelCoordinate = "";
+			int dividend = y;
+			int modulo;
+
+			while (dividend > 0)
+			{
+				modulo = (dividend - 1) % 26;
+				ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+				dividend = (int)((dividend - modulo) / 26);
+			}
+			ExcelCoordinate += x.ToString();
+
+			return ExcelCoordinate;
+		}
+
+
 	}
 }
