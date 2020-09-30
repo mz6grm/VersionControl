a637d49638db7f7fb3768b351598f3635c3af57a
Csaba Kaló <kalo.csk@gmail.com>
2020-09-30 17:53:33 +0200

    second project fourth task

--- negyedik/negyedik/Form1.cs
+++ negyedik/negyedik/Form1.cs
@@ -12,9 +12,16 @@ namespace negyedik
 {
 	public partial class Form1 : Form
 	{
+		RealEstateEntities context = new RealEstateEntities();
+		List<Flat> Flats;
 		public Form1()
 		{
 			InitializeComponent();
+			LoadData();
+		}
+		private void LoadData()
+		{
+			Flats = context.Flat.ToList();
 		}
 	}
 }
