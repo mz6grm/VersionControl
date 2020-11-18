aeb3355264c0339eb9b3346d5ed21c2c92dfd7a0
Csaba Kaló <kalo.csk@gmail.com>
2020-11-18 18:55:57 +0100

    commit 4,5,6,7

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+	public class AccountControllerTestFixture
+	{
+		[Test,
+		TestCase("abcd1234", false),
+		TestCase("irf@uni-corvinus", false),
+		TestCase("irf.uni-corvinus.hu", false),
+		TestCase("irf@uni-corvinus.hu", true)]
+		public void TestValidateEmail(string email, bool expectedResult)
+		{
+			// Arrange
+			var accountController = new AccountController();
+
+			// Act
+			var actualResult = accountController.ValidateEmail(email);
+
+			// Assert
+			Assert.AreEqual(expectedResult, actualResult);
+		}
+				
+		[Test,
+		TestCase("Abcd", false),
+		TestCase("ABCD", false),
+		TestCase("abcd", false),
+		TestCase("Ab1", false),
+		TestCase("abcd1234", true)]
+		public void TestValidatePassword(string password, bool expectedResult)
+		{
+			// Arrange
+			var accountController = new AccountController();
+
+			// Act
+			var actualResult = accountController.ValidatePassword(password);
+
+			// Assert
+			Assert.AreEqual(expectedResult, actualResult);
+		}
+
+		public void Password_Validate()
+		{
+
+		}
+	}
+}
