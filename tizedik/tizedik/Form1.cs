6897c934630565c3f03b97d094f0fd6e8e46aa56
Csaba Kaló <kalo.csk@gmail.com>
2020-11-25 18:28:01 +0100

    commit 1 again

--- tizedik/tizedik/Form1.cs
+++ tizedik/tizedik/Form1.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace tizedik
 {
 	public partial class Form1 : Form
 	{
+		GameController gc = new GameController();
+		GameArea ga;
+
+		int populationSize = 100;
+		int nbrOfSteps = 10;
+		int nbrOfStepsIncrement = 10;
+		int generation = 1;
 		public Form1()
 		{
-			InitializeComponent();
+			InitializeComponent();			
+			ga = gc.ActivateDisplay();
+			this.Controls.Add(ga);
+
+			gc.AddPlayer();
+			gc.GameOver += Gc_GameOver;
+
+			for (int i = 0; i < populationSize; i++)
+			{
+				gc.AddPlayer(nbrOfSteps);
+			}
+			gc.Start();
+		}
+
+		private void Gc_GameOver(object sender)
+		{
+			generation++;
+			label1.Text = string.Format(
+				"{0}. generáció",
+				generation);
+
+			var playerList = from p in gc.GetCurrentPlayers()
+							 orderby p.GetFitness() descending
+							 select p;
+			var topPerformers = playerList.Take(populationSize / 2).ToList();
 		}
 	}
 }
